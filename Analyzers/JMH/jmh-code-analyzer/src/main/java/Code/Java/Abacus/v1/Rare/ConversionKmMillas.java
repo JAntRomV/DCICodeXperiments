a5529538f5657c5d76192a9b45d254724184d0b0
JAntRomV <jantonioromerov@gmail.com>
2026-05-03 01:51:49 -0600

    deleting extra classes

--- Analyzers/JMH/jmh-code-analyzer/src/main/java/Code/Java/Abacus/v1/Rare/ConversionKmMillas.java
+++
@@ -1,9 +0,0 @@
-package Code.Java.Abacus.v1.Rare;
-
-public class ConversionKmMillas {
-    public static void main(String[] args) {
-        double kilometros = 100;
-        double millas = kilometros * 0.621371;
-        System.out.println(kilometros + " km = " + millas + " millas");
-    }
-}
--- Analyzers/JMH/jmh-code-analyzer/src/main/java/Code/Java/Abacus/v1/Rare/ValorAbsoluto.java
+++
@@ -1,9 +0,0 @@
-package Code.Java.Abacus.v1.Rare;
-
-public class ValorAbsoluto {
-    public static void main(String[] args) {
-        int numero = -42;
-        int valorAbsoluto = Math.abs(numero);
-        System.out.println("El valor absoluto de " + numero + " es: " + valorAbsoluto);
-    }
-}
