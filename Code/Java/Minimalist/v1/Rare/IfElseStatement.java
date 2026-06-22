545c5cfeaab027a9b1eb89ece5ddf82c770e2770
JAntRomV <jantonioromerov@gmail.com>
2026-06-22 13:26:09 -0600

    adding new classes

--- Code/Java/Minimalist/v1/Rare/IfElseStatement.java
+++ Code/Java/Minimalist/v1/Rare/IfElseStatement.java
@@ -2,9 +2,16 @@ package Code.Java.Minimalist.v1.Rare;
 
 public class IfElseStatement {
     public static void main(String[] args) {
+        long begin = System.nanoTime();
+        System.out.println(begin);
         int value = 0;
+        System.out.println(System.nanoTime());
         if (value > 0) {
+            System.out.println(System.nanoTime());
         } else {
+            System.out.println(System.nanoTime());
         }
+        long end = System.nanoTime();
+        System.out.println(end-begin);
     }
 }
