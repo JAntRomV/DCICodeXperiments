3438bc90b5ea3670453fc7ec0bec8f6effaf5a11
JAntRomV <jantonioromerov@gmail.com>
2026-06-25 11:54:05 -0600

    adding new jmh methods project

---
+++ Analyzers/JMH/jmh-methods/src/main/java/com/dci/MainLauncher.java
@@ -0,0 +1,21 @@
+// package com.dci;
+
+// import org.openjdk.jmh.runner.Runner;
+// import org.openjdk.jmh.runner.options.Options;
+// import org.openjdk.jmh.runner.options.OptionsBuilder;
+// import org.openjdk.jmh.results.format.ResultFormatType;
+// import org.openjdk.jmh.profile.GCProfiler;
+// import java.util.Set;
+// import java.util.HashSet; 
+
+
+// public class MainLauncher {
+//     public static void main(String[] args) throws Exception {
+//         Options opt = new OptionsBuilder()
+//                 .include(MyBenchmark.class.getSimpleName())
+//                 .addProfiler(GCProfiler.class)
+//                 .build();
+
+//         new Runner(opt).run();
+//     }
+// }
---
+++ Analyzers/JMH/jmh-methods/src/main/java/com/dci/StringBenchmark.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Thread)
+public class StringBenchmark {
+
+    @Param({"10", "100"})
+    public int iterations;
+
+    @Benchmark
+    public void testStringPlus(Blackhole bh) {
+        String str = "";
+        for (int i = 0; i < iterations; i++) {
+            str += "data";
+        }
+        bh.consume(str);
+    }
+
+    @Benchmark
+    public void testStringBuilder(Blackhole bh) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < iterations; i++) {
+            sb.append("data");
+        }
+        bh.consume(sb.toString());
+    }
+}
